4e646821c8522792f603bf984b43d4ea964e9e3c
Dmitriy B. <fessdemon@gmail.com>
2024-10-08 18:29:11 +0300

    python lab 4

--- python/lab_4_1.py
+++ python/lab_4_1.py
@@ -1,29 +1,67 @@
-# Чтение англо-русского словаря из файла
-input_file = 'task_4_1/input.txt'
-output_file = 'task_4_1/output.txt'
+import os
+import re
+
+# Инициализация рабочего каталога
+print("Введите имя каталога, содержащего файл input.txt:")
+name_dir = input()
+# name_dir = 'task_4_1'
+
+# Проверка корректности рабочего каталога
+if not (os.path.exists(name_dir)):
+    print("Такого каталога не существует. Введите корректно полное имя каталога: ")
+    name_dir = input()
+
+# Инициализация файлов
+input_file = name_dir + "/input.txt"
+output_file = name_dir + "/output.txt"
+
+# Проверка входного файла
+while not (os.path.exists(input_file)):
+    print(
+        "Файла input.txt  в указанном каталоге не существует. Введите корректно полное имя входного файла: "
+    )
+    input_file = input()
 
 # Словарь для хранения русско-английских переводов
 rus_eng_dict = {}
 
-# Открываем файл для чтения
-with open(input_file, 'r', encoding='utf-8') as file:
-    for line in file:
-        # Разделяем строку на английское слово и русские переводы
-        eng_word, rus_words = line.strip().split(' – ')
-        rus_words_list = [word.strip() for word in rus_words.split(',')]
+# Чтение англо-русского словаря из файла
+if os.stat(input_file).st_size == 0:
+    print("Файл словаря пуст")
+else:
+    # Открываем файл для чтения
+    with open(input_file, "r", encoding="utf-8") as file:
+        for line in file:
+            # Убираем лишние символы и пробелы с краев строки
+            clean_line = re.sub(r"[^\w\s–,]", "", line).strip()
 
-        # Заполняем русско-английский словарь
-        for rus_word in rus_words_list:
-            if rus_word not in rus_eng_dict:
-                rus_eng_dict[rus_word] = []
-            rus_eng_dict[rus_word].append(eng_word)
+            # Проверка, содержит ли строка цифры
+            if re.search(r"\d", clean_line):
+                # Удаляем цифры из строки, оставляя только буквы
+                clean_line = re.sub(r"\d+", "", clean_line).strip()
 
-# Сортируем словарь по ключам (русским словам)
-sorted_rus_eng_dict = dict(sorted(rus_eng_dict.items()))
+            # Разделяем строку на английское слово и русские переводы
+            if " – " in clean_line:
+                eng_word, rus_words = clean_line.split(" – ", 1)
+                # Очищаем список русских переводов от лишних пробелов и мусорных символов
+                rus_words_list = [
+                    re.sub(r"[^\w\s]", "", word.strip())
+                    for word in rus_words.split(",")
+                    if word.strip()
+                ]
 
-# Записываем результат в выходной файл
-with open(output_file, 'w', encoding='utf-8') as file:
-    for rus_word, eng_words in sorted_rus_eng_dict.items():
-        file.write(f"{rus_word} – {', '.join(eng_words)}\n")
+                # Заполняем русско-английский словарь
+                for rus_word in rus_words_list:
+                    if rus_word not in rus_eng_dict:
+                        rus_eng_dict[rus_word] = []
+                    rus_eng_dict[rus_word].append(eng_word)
 
-print("Русско-английский словарь успешно создан и сохранен в output.txt.")
+    # Сортируем словарь по ключам (русским словам)
+    sorted_rus_eng_dict = dict(sorted(rus_eng_dict.items()))
+
+    # Записываем результат в выходной файл
+    with open(output_file, "w", encoding="utf-8") as file:
+        for rus_word, eng_words in sorted_rus_eng_dict.items():
+            file.write(f"{rus_word} – {', '.join(eng_words)}\n")
+
+    print("Русско-английский словарь успешно создан и сохранен в output.txt.")
---
+++ python/lab_4_6.py
@@ -0,0 +1,79 @@
+import os
+import re
+
+input_file_1 = "task_4_6/shop1.txt"
+input_file_2 = "task_4_6/shop2.txt"
+output_file = "task_4_6/shop_max.txt"
+
+# Проверка входного файла shop1.txt
+while not (os.path.exists(input_file_1)):
+    print(
+        "Файла shop1.txt  в указанном каталоге не существует. Введите корректно полное имя входного файла: "
+    )
+    input_file_1 = input()
+
+# Проверка входного файла shop2.txt
+while not (os.path.exists(input_file_2)):
+    print(
+        "Файла shop2.txt  в указанном каталоге не существует. Введите корректно полное имя входного файла: "
+    )
+    input_file_2 = input()
+
+# Словарь для хранения прайс-листов
+prices_dict = {}
+
+
+# Функция для очистки строки от мусора
+def clean_line(line):
+    # Регулярное выражение ищет строку формата "название товара - цена", где цена — это число
+    match = re.match(r"(.+?)\s*-\s*(\d+)", line)
+    if match:
+        name = match.group(1).strip()  # Убираем лишние пробелы вокруг названия
+        price = match.group(2)  # Получаем цену
+        return name, price
+    return None
+
+
+# Открываем 1-ый файл для чтения
+with open(input_file_1, "r", encoding="utf-8") as file:
+    for line in file:
+        result = clean_line(line.rstrip())  # Очищаем строку
+        if result:
+            name, price = result
+            prices_dict[name] = price
+
+
+# Открываем 2-ый файл для чтения
+with open(input_file_2, "r", encoding="utf-8") as file:
+    for line in file:
+        result = clean_line(line.rstrip())
+        if result:
+            name, price = result
+            if name not in prices_dict:
+                prices_dict[name] = price
+            else:
+                prices_dict[name] = max(prices_dict[name], price)
+
+
+def write_to_file(filename, data):
+    """Записывает данные в файл."""
+    with open(filename, "w", encoding="utf-8") as file:
+        for name, price in sorted(data.items()):
+            file.write(f"{name} – {price}\n")
+    print(f"Файл {filename} успешно записан.")
+
+
+# Проверка существования файла
+if os.path.exists(output_file):
+    print(
+        f"Файл {output_file} в указанном каталоге уже существует. Введите OK, если хотите переписать имеющийся файл, или введите любое другое значение для отмены: "
+    )
+    is_read = input()
+    if is_read != "OK":
+        print("Перезапись файла отменена.")
+    else:
+        # Перезапись файла
+        write_to_file(output_file, prices_dict)
+else:
+    # Если файла нет, то записываем данные в новый файл
+    write_to_file(output_file, prices_dict)
