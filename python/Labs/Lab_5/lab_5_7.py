4738c5d0b548b06064acf54b720ef05dd53b6125
Dmitriy B. <fessdemon@gmail.com>
2024-10-16 14:23:16 +0300

    update python lab 5

--- python/Labs/Lab_5/lab_5_7.py
+++ python/Labs/Lab_5/lab_5_7.py
@@ -1,24 +1,27 @@
 import numpy as np
 
 # 1. Создание массива
-rows, cols = 3, 5
-A = np.random.randint(-9, 6, size=(rows, cols))
-print("Исходный массив:")
-print(A)
+rows = 5
+cols = 5
+low = -10
+high = 10
+
+matrix = np.random.randint(low, high, size=(rows, cols))
+print(matrix)
 
 # 2. Поиск минимального элемента
-min_elem = np.min(A)
-min_index = np.unravel_index(np.argmin(A), A.shape)
+min_elem = np.min(matrix)
+min_index = np.unravel_index(np.argmin(matrix), matrix.shape)
 min_row, min_col = min_index
 
 # 3. Удаление строки и столбца
-A_reduced = np.delete(A, min_row, axis=0)  # Удаляем строку
-A_reduced = np.delete(A_reduced, min_col, axis=1)  # Удаляем столбец
+matrix_reduced = np.delete(matrix, min_row, axis=0)  # Удаляем строку
+matrix_reduced = np.delete(matrix_reduced, min_col, axis=1)  # Удаляем столбец
 print("Массив после удаления строки и столбца:")
-print(A_reduced)
+print(matrix_reduced)
 
 # 4. Сортировка оставшихся столбцов по неубыванию их минимальных элементов
-sorted_indices = np.argsort(np.min(A_reduced, axis=0))
-A_sorted = A_reduced[:, sorted_indices]
+sorted_indices = np.argsort(np.min(matrix_reduced, axis=0))
+matrix_sorted = matrix_reduced[:, sorted_indices]
 print("Отсортированный массив по минимальным элементам столбцов:")
-print(A_sorted)
+print(matrix_sorted)
