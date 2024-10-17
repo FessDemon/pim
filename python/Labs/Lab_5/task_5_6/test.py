c5d4f3e8ad329cc3fb8959adc4ffa87e53d37efe
Dmitriy B. <fessdemon@gmail.com>
2024-10-17 13:39:20 +0300

    update lab 6

--- python/Labs/Lab_5/task_5_6/test.py
+++ python/Labs/Lab_5/task_5_6/test.py
@@ -1,16 +1,13 @@
 import numpy as np
 
-# Создаем пустую матрицу размером 6x6
-matrix = np.zeros((6, 6))
+n = 4
+m = 4
+k = 1
+# Создаем пустую матрицу размером n x m
+matrix = np.zeros((n, m))
 
-# Первая строка заполняется числами от 1 до 6
-matrix[0] = np.arange(1, 7)
+for i in range(0, n+1):
+    for j in range(0, m+1):
+        matrix[i][j] += i
 
-# Остальные строки начинаютсья с числа, следующего за максимальным числом предыдущей строки
-max_value = matrix[0].max() + 1
-for i in range(1, 6):
-    next_row = max_value + np.arange(i+1)
-    matrix[i] = next_row[:6-(i*2)]
-    max_value += len(next_row)
-    
 print(matrix)
---
+++ python/Labs/Lab_6/lab_6_1.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+df_build = pd.DataFrame("moscow-buildings.csv")
