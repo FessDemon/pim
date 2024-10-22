549b819822e2aa6d8068186d32b6530244e89908
Dmitriy B. <fessdemon@gmail.com>
2024-10-22 09:04:37 +0300

    update labs 6

--- python/Labs/Lab_6/lab_6_1.py
+++ python/Labs/Lab_6/lab_6_1.py
@@ -1,3 +1,5 @@
 import pandas as pd
+from datetime import date
 
-df_build = pd.DataFrame("moscow-buildings.csv")
+df_build = pd.read_csv("python/Labs/Lab_6/lab_6_1.py", index_col=0)
+df_build.info()
